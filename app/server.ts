311f083d9d747c47dd7500037dce595363b91645
Renan Moreira Gomes <renangomes.es@gmail.com>
2023-02-23 07:48:58 -0300

    initial setup

---
+++ app/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    port: Number(env.PORT),
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('Server is running')
+  })
